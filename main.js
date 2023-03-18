55e319a401ddb03cf4e4758176404327c80f46d4
chika918 <113702101+chika918@users.noreply.github.com>
2023-03-18 09:47:18 +0900

    finally処理追加

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 //JS課題
 //try catchを使ってエラー文をコンソールに表示させる
 
-let array = [0,1,2,3,4];
-
 try{
+    let array = [0,1,2,3,4];
     for(let i = 0; i <= 5; i++){
         console.log(array2[i]);
     }
 } catch(e){
+    //エラーが発生したら実行される
     console.log("エラーを検知しました");
+} finally{
+    //エラーに関係なく必ず実行
+    console.log("必ず実行");
 }
