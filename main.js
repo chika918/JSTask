480af5face1a24d5cc81770cc6550f377ea2f979
chika918 <113702101+chika918@users.noreply.github.com>
2023-03-19 01:21:49 +0900

    問2＿追記

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 //JS課題
-//try catchを使ってエラー文をコンソールに表示させる
-
+//問1：try catchを使ってエラー文をコンソールに表示させる
 try{
     let array = [0,1,2,3,4];
     for(let i = 0; i <= 5; i++){
@@ -8,8 +7,32 @@ try{
     }
 } catch(e){
     //エラーが発生したら実行される
-    console.log("エラーを検知しました");
+    console.log("エラーを検知しました（問1）");
 } finally{
     //エラーに関係なく必ず実行
-    console.log("必ず実行");
+    console.log("必ず実行（問1）");
 }
+
+//問2：throw
+try{
+    let array = [0,1,2,3,4];
+
+    //該当する場合強制でエラーを発生する
+    if (array.length === 5){
+        throw('エラーが発生しました')
+    }
+
+    //上記以外の場合は処理が実行される
+    for(let i = 0; i <= 5; i++){
+        console.log(array[i]);
+    }
+
+} catch(e){
+    //エラーが発生したら実行される
+    console.log(e);
+} finally{
+    //エラーに関係なく必ず実行
+    console.log("必ず実行（問2）");
+}
+
+
