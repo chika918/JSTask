39f52ca8d2079ce7c1ed75ae774912e659d502d5
chika918 <113702101+chika918@users.noreply.github.com>
2023-03-18 09:30:31 +0900

    JS課題＿try catch

---
+++ main.js
@@ -0,0 +1,12 @@
+//JS課題
+//try catchを使ってエラー文をコンソールに表示させる
+
+let array = [0,1,2,3,4];
+
+try{
+    for(let i = 0; i <= 5; i++){
+        console.log(array2[i]);
+    }
+} catch(e){
+    console.log("エラーを検知しました");
+}
