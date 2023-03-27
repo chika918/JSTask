301c3b5fa670dff2c0e8584ad48fd052a66ec749
chika918 <113702101+chika918@users.noreply.github.com>
2023-03-28 01:19:31 +0900

    郵便番号検索＿メモ追加

--- postcode.js
+++ postcode.js
@@ -10,7 +10,11 @@ search.addEventListener('click', function(){
     let url = api + input;
     //console.log(url);
 
-    fetchJsonp(url, {
+    //メモ
+    //fetchJsonpでも実行可能
+
+    //fetchで郵便番号APIと通信
+    fetch(url, {
         timeout: 10000, //タイムアウト時間
     })
     .then((response)=>{
@@ -18,31 +22,27 @@ search.addEventListener('click', function(){
         return response.json();  
     })
     .then((data)=>{
-        console.log(data);
-        if(data.status === 400){ //エラー時
-            console.log(data.message);
+        //console.log(data);
+        //エラー種別を通信結果から判定して振り分け
+        if(data.message == 'パラメータ「郵便番号」の桁数が不正です。'){
+            alert('郵便番号は数字7桁で入力してください');
+        } else if(data.status === 400){
             alert('郵便番号を指定してください。');
-        } else if(data.message = 'パラメータ「郵便番号」の桁数が不正です。'){
-            alert = ('aaa');
         } else if(data.results === null){
-            alert = ('郵便番号から住所が見つかりませんでした。');
+            alert('郵便番号から住所が見つかりませんでした。郵便番号を確認してください。');
         } else {
+            //オブジェクトのaddress1〜3のデータを反映
             address1.value = data.results[0].address1;
             address2.value = data.results[0].address2;
             address3.value = data.results[0].address3;
         }
     })
-
-
-
-
     .catch((ex)=>{
-        console.log(ex);
+        alert('予期せぬエラーです');
     });
 },false);
 
 let clear = document.getElementById('clear');
-
 clear.addEventListener('click', function(){
     input.value = "";
     address1.value = "";
