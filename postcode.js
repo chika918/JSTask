ae77f94358fdda06f9ce860c4ac4b4c6180cda18
chika918 <113702101+chika918@users.noreply.github.com>
2023-03-29 13:24:24 +0900

    課題1修正、課題5(郵便番号検索)提出

--- postcode.js
+++ postcode.js
@@ -10,24 +10,29 @@ search.addEventListener('click', async function(){
     let address2 = document.getElementById('address2');
     let address3 = document.getElementById('address3');
 
-    //fetchで郵便番号APIと通信(メモ：fetchJsonpでも実行可能)
+    //fetchで郵便番号APIと通信
     const response = await fetch(url);
     const data = await response.json();
     
     //エラー種別を通信結果から判定して振り分け
-    if(data.message == 'パラメータ「郵便番号」の桁数が不正です。'){
-        alert('郵便番号は数字7桁で入力してください');
-    } else if(data.status === 400){
-        alert('郵便番号を指定してください。');
-    } else if(data.results === null){
-        alert('郵便番号から住所が見つかりませんでした。郵便番号を確認してください。');
-    } else {
-        //オブジェクトのaddress1〜3のデータを反映
-        address1.value = data.results[0].address1;
-        address2.value = data.results[0].address2;
-        address3.value = data.results[0].address3;
+    try {
+        if(data.message == 'パラメータ「郵便番号」の桁数が不正です。'){
+            alert('郵便番号は数字7桁で入力してください');
+        } else if(data.status === 400){
+            alert('郵便番号を指定してください。');
+        } else if(data.results === null){
+            alert('郵便番号から住所が見つかりませんでした。郵便番号を確認してください。');
+        } else {
+            //オブジェクトのaddress1〜3のデータを反映
+            address1.value = data.results[0].address1;
+            address2.value = data.results[0].address2;
+            address3.value = data.results[0].address3;
+        }
+    } catch(e){
+        console.log(e);
+        alert('エラーが発生しました。エラー内容：' + e.message);
     }
-},false);
+});
 
 //入力値クリア
 let clear = document.getElementById('clear');
